7f2451b1e9b22247bf9b864b5b830196fe4a8efe
Zhengtong Liu <zhengtongliu@outlook.com>
2020-11-16 17:44:05 +0800

    update HW5

---
+++ assign5/randall/rand64-hw.h
@@ -0,0 +1,13 @@
+struct cpuid;
+struct cpuid
+cpuid (unsigned int leaf, unsigned int subleaf);
+_Bool rdrand_supported (void);
+void
+hardware_rand64_init (void);
+unsigned long long
+hardware_rand64 (void);
+void
+hardware_rand64_fini (void);
+
+
+
